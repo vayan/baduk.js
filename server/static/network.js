8d2576ffaa5d93770a3ff846cb14cb9b1c635101
vayan <vaillant.y@gmail.com>
2013-07-10 22:23:08 +0200

    signaling over websocket kinda worrking

--- server/static/network.js
+++ server/static/network.js
@@ -1,7 +1,7 @@
 var ws;
 var conn_started = false;
-var peerConn = null;
-var dataConn = null;
+var peerConn;
+var dataConn;
 var attachConnectionListeners = null;
 var cfg = {
     "iceServers": [{
@@ -25,103 +25,161 @@ if (ws != null) {
 
 ws = new WebSocket("ws://localhost:8080/ws");
 
-function logg(s) {
-    console.log(s);
-}
-
 function connect() {
-    if (!conn_started) {
-        log("Starting stuff");
-        createPeerConnection();
-        conn_started = true;
-    } else {
-        log("already connected");
-    }
+    log("Starting stuff");
+    createPeerConnection();
 }
 
 function onSignal(message) {
     log("Sending setup signal");
-    ws.send(message);
+   sendMessage(message);
 }
 
+function createDataConnection() {
+    try {
+        dataConn = peerConn.createDataChannel('test', {
+            reliable: true
+        });
+        log("create data chan");
+        dataConn.onmessage = function(e) {
+            log("got message datacon", e.data);
+        };
+        dataConn.onopen = function(e) {
+            log("open datacon", e.data);
+        };
+    } catch (e) {
+        log("No data channel, error " , e);
+    }
+}
 
-function createPeerConnection() {
+function makeAnswer() {
+  peerConn.createAnswer(function(answer) {
+    log('Created answer.');
+    peerConn.setLocalDescription(answer, function() {
+      log('Set localDescription to answer.');
+      sendMessage(answer);
+    }, function(err) {
+      log('Failed to setLocalDescription, ', err);
+    });
+  }, function(err) {
+    log('Failed to create answer, ', err);
+  });
+};
+
+
+function makeOffer() {
+     log('starting create offer...');
+    peerConn.createOffer(function(offer) {
+        log('Created offer.', offer);
+        peerConn.setLocalDescription(offer, function() {
+            log('Set localDescription to offer');
+           // sendMessage({
+           //      type: 'OFFER',
+           //      payload: offer
+           //  });
+        sendMessage(offer);
+        }, function(err) {
+            log('Failed to setLocalDescription, ', err);
+        });
+    }, function(err) {
+        log('Failed to createOffer, ', err);
+    });
+};
+
+function createPeerConnection(isServer) {
     try {
         log("_creating_ peer connection..");
-        peerConn = new RTCPeerConnection(cfg, onSignal);
-        log(peerConn);
-        peerConn.onicecandidate = function(e) {
-            log("ICE server");
-            if (e.candidate) {
-                document.localICECandidateForm.localICECandidate.value = JSON.stringify(e.candidate);
-            }
+        peerConn = new RTCPeerConnection(cfg, {
+            optional: [{
+                    RtpDataChannels: true
+                }
+            ]
+        });
+        //log(peerConn);
+
+        dataConn = peerConn.createDataChannel('test', {
+           reliable: true
+        })
+        log("create data chan");
+        dataConn.onmessage = function(e) {
+            log("got message datacon", e.data);
+        };
+        dataConn.onopen = function(e) {
+            log("open datacon", e.data);
         };
 
+
+          peerConn.onicecandidate = function(e) {
+            log("receive ICE server", e);
+            //send CANDIDATE to ws
+            if (e.candidate) {
+                sendMessage(JSON.stringify({ "candidate": evt.candidate }));
+            }
+        }
+        
+        peerConn.oniceconnectionstatechange = function() {
+            if ( !! self.pc && self.pc.iceConnectionState === 'disconnected') {
+                log('iceConnectionState is disconnected');
+            }
+        };
         peerConn.onconnection = function(e) {
             log("connected");
         }
 
-        peerConn.createOffer(function(offerDesc) {
-            log("Created local offer" + offerDesc);
-            peerConn.setLocalDescription(offerDesc);
-            ws.send(JSON.stringify(offerDesc));
-        }, function() {
-            log("Couldn't create offer");
-        });
-
-        peerConn.createAnswer(function(answer) {
-            log('Created answer.');
-            peerConn.setLocalDescription(answer, function() {
-                log('Set localDescription to answer.');
-            });
-        });
-
-        log('listening for data channel..');
         peerConn.ondatachannel = function(evt) {
             log('received data channel !');
         }
 
-        try {
-            dataConn = peerConn.createDataChannel('test', {
-                reliable: true
-            });
-            log("create data chan");
-            dataConn.onmessage = function(e) {
-                log("got message datacon", e.data);
-            };
-            dataConn.onopen = function(e) {
-                log("open datacon", e.data);
-            };
-        } catch (e) {
-            log("No data channel (pc1)" + e);
-        }
+        if (isServer)
+            makeOffer();
+        //else
+          //makeAnswer();
+
+      
+
+    
     } catch (e) {
-        log("Failed to create PeerConnection, exception: " + e.message);
+        log("Failed to create PeerConnection, error: ", e.message);
     }
 }
 
 function sendMessage(s) {
-    var channel = new RTCMultiSession();
-    channel.send({
-        message: s
-    });
-    return false;
+    s  = JSON.stringify(s);
+    log("_WS_ Sending : ", s);
+   ws.send(s);
 };
 
 ws.onopen = function() {
-    log("ws open");
-    connect();
+    log("_WS_ open");
+    //connect();
 };
 
 ws.onmessage = function(e) {
-    log("RECEIVED: " + e.data);
-    //createPeerConnection();
-    log('Processing signaling message...');
-    var desc = new RTCSessionDescription(JSON.parse(e.data));
-    peerConn.setRemoteDescription(desc);
+    if (!peerConn) {
+        createPeerConnection(false);
+    }
 
+    log("_WS_ RECEIVED: ", e);
+    var jsone = JSON.parse(e.data);
+
+    if (jsone.sdp) {
+        log('Added remote descr');
+        peerConn.setRemoteDescription(new RTCSessionDescription(jsone.sdp), function(){
+            makeAnswer();
+        });
+    } 
+    else {
+        log('Added ICE candidate.');
+        peerConn.addIceCandidate(new RTCIceCandidate(jsone.candidate));
+    }
 };
 
 ws.onclose = function(e) {
-    log("no ws");
+    log("_WS_ closed");
 };
+
+$(document).ready(function() {
+    $("#foo").click(function() {
+        createPeerConnection(true);
+    });
+});

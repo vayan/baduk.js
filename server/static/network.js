92c31d504ab21f84c63b7097f296ae92f882a5f2
vayan <yann@vaillant.im>
2013-07-11 11:04:06 +0200

    fix error syntax

--- server/static/network.js
+++ server/static/network.js
@@ -1,9 +1,8 @@
 var ws;
-var conn_started = false;
 var peerConn;
 var dataConn;
-var datacon2;
-var attachConnectionListeners = null;
+var dataConn2;
+var logtype = "*NETWORK* : ";
 
 var cfg = {
     "iceServers": [{
@@ -27,77 +26,68 @@ if (ws != null) {
 
 ws = new WebSocket("ws://localhost:8080/ws");
 
-function newConnec(id) {
+function newConnection(id) {
     createPeerConnection();
     listenICEcandidates();
-    setupNegotiationHandler();
     listenDataChan();
     createDataConnection();
 }
 
 function createPeerConnection() {
-    log("Creating peer Connectin");
+    log(logtype+ "Creating RTCPeerConnection");
     peerConn = new RTCPeerConnection(cfg, {
-            optional: [{
-                    RtpDataChannels: true
-                }
-            ]
-        });
+        optional: [{
+                RtpDataChannels: true
+            }
+        ]
+    });
 }
 
 function listenICEcandidates() {
-    log("listening ICEcandidates");
+    log(logtype+ "Listening for ICEcandidates");
     peerConn.onicecandidate = function(e) {
-            log("receive ICE server", e);
-            //send CANDIDATE to ws
-            if (e.candidate) {
-                sendMessage(JSON.stringify({ "candidate": evt.candidate }));
-            }
+        log(logtype+ "Received ICE server, ", e);
+        if (e.candidate) {
+            sendMessage(JSON.stringify({
+                "candidate": evt.candidate
+            }));
         }
+    }
 }
 
 function listenDataChan() {
-    log("listening datachan");
- peerConn.ondatachannel = function(evt) {
-    log('Received data channel', evt);
-     datacon2 = evt.channel;
-     datacon2.onmessage = function(e) {
-            log("got message datacon", e);
+    log(logtype+ "Listening for datachannel");
+    peerConn.ondatachannel = function(evt) {
+        log(logtype+ 'Received datachannel, ', evt);
+        dataConn2 = evt.channel;
+        dataConn2.onmessage = function(e) {
+            log(logtype+ "got message datacon", e);
         };
-        datacon2.onopen = function(e) {
-            log("open datacon", e);
-            datacon2.send("testeeee");
+        dataConn2.onopen = function(e) {
+            log(logtype+ "open datacon", e);
         };
-        datacon2.onclose = function (e) {
-            log("datacon closed", e);
+        dataConn2.onclose = function(e) {
+            log(logtype+ "datacon closed", e);
         }
-  };
-}
-
-function setupNegotiationHandler() {
-  log('Listening for negotiationneeded');
-  peerConn.onnegotiationneeded = function() {
-   log('`negotiationneeded` triggered');
-    //makeOffer();
-  };
+    };
 }
 
 function makeOffer() {
-     log('starting create offer...');
+    log(logtype+ 'Starting to create offer...');
     peerConn.createOffer(function(offer) {
-        log('Created offer.', offer);
+        log(logtype+ 'Offer created, ', offer);
         peerConn.setLocalDescription(offer, function() {
-            log('Set localDescription to offer');
-           // sendMessage({
-           //      type: 'OFFER',
-           //      payload: offer
-           //  });
-        sendMessage(offer);
+            log(logtype+ 'Set localDescription to offer');
+            // sendMessage({
+            //      type: 'OFFER',
+            //      payload: offer
+            //  });
+            sendMessage(offer);
         }, function(err) {
-            log('Failed to setLocalDescription, ', err);
+            log(logtype+ 'Failed to setLocalDescription, ', err);
         });
     }, function(err) {
-        log('Failed to createOffer, ', err);
+        log(logtype+ 'Failed to createOffer, ', err);
     });
 }
 
@@ -106,64 +96,65 @@ function createDataConnection() {
         dataConn = peerConn.createDataChannel('test', {
             reliable: true
         });
-        log("create data chan");
+        log(logtype+ "Create DataChannel");
         dataConn.onmessage = function(e) {
-            log("got message datacon", e.data);
+            log(logtype+ "got message datacon", e.data);
         };
         dataConn.onopen = function(e) {
-            log("open datacon", e.data);
+            log(logtype+ "open datacon", e.data);
         };
-        dataConn.onclose = function (e) {
-            log("datacon closed", e);
+        dataConn.onclose = function(e) {
+            log(logtype+ "datacon closed", e);
         }
     } catch (e) {
-        log("No data channel, error " , e);
+        log(logtype+ "Error DataChannel, ", e);
     }
 }
 
 function makeAnswer() {
-  peerConn.createAnswer(function(answer) {
-    log('Created answer.');
-    peerConn.setLocalDescription(answer, function() {
-      log('Set localDescription to answer.');
-      sendMessage(answer);
+    peerConn.createAnswer(function(answer) {
+        log(logtype+ 'Created answer');
+        peerConn.setLocalDescription(answer, function() {
+            log(logtype+ 'Set localDescription to answer');
+            sendMessage(answer);
+        }, function(err) {
+            log(logtype+ 'Failed to setLocalDescription, ', err);
+        });
     }, function(err) {
-      log('Failed to setLocalDescription, ', err);
+        log(logtype+ 'Failed to create answer, ', err);
     });
-  }, function(err) {
-    log('Failed to create answer, ', err);
-  });
 }
 
 function handleSDP(sdp, type) {
     //check why must sdp and not sdp.sdp from json..
-  sdp = new RTCSessionDescription(sdp);
+    sdp = new RTCSessionDescription(sdp);
 
-  peerConn.setRemoteDescription(sdp, function() {
-    log('Set remoteDescription: ' , type);
-    if (type === 'OFFER') {
-      makeAnswer();
-    }
-  }, function(err) {
-    log('Failed to setRemoteDescription, ', err);
-  });
+    peerConn.setRemoteDescription(sdp, function() {
+        log(logtype+ 'Set RemoteDescription, ', type);
+        if (type === 'OFFER') {
+            makeAnswer();
+        }
+    }, function(err) {
+        log(logtype+ 'Failed to setRemoteDescription, ', err);
+    });
 }
 
 function sendMessage(s) {
-    s  = JSON.stringify(s);
-    log("_WS_ Sending : ", s);
-   ws.send(s);
+    s = JSON.stringify(s);
+    log(logtype+ " _WS_ Sending : ", s);
+    ws.send(s);
 };
 
 ws.onopen = function() {
-    log("_WS_ open");
+    log(logtype+ "_WS_ Open");
     //connect();
 };
 
 ws.onmessage = function(e) {
+    log(logtype+ " _WS_ Received message, ", e.data);
     var jsone = JSON.parse(e.data);
     if (!peerConn) {
-        newConnec();
+        newConnection();
         handleSDP(jsone, 'OFFER');
     } else {
         handleSDP(jsone, 'ANSWER');
@@ -171,15 +162,16 @@ ws.onmessage = function(e) {
 };
 
 ws.onclose = function(e) {
-    log("_WS_ closed");
+    log(logtype+ " _WS_ Closed");
 };
 
 $(document).ready(function() {
-    $("#foo").click(function() {
-        newConnec();
+    $("#start").click(function() {
+        newConnection();
         makeOffer();
     });
-    $("#bar").click(function(){
-        createDataConnection();
+    $("#send").click(function() {
+        dataConn.send($("#msg").val());
+        dataConn2.send($("#msg").val());
     });
 });
